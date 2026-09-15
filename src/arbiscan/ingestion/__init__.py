db27bde02464431d255fb8b5755d8a99cd1b8da2
LorFerSilve <silvetnefernandezlorenzo@gmail.com>
2026-09-15 04:56:56 +0200

    feat: export realtime ingestion primitives

--- src/arbiscan/ingestion/__init__.py
+++ src/arbiscan/ingestion/__init__.py
@@ -6,10 +6,38 @@ from arbiscan.ingestion.collector import (
     IngestionIssue,
     collect_snapshots,
 )
+from arbiscan.ingestion.realtime import (
+    LiveQuoteStore,
+    ProviderIngestionHealth,
+    ProviderPollMetrics,
+    ProviderRateGate,
+    QuoteKey,
+    QuoteStoreApplyResult,
+    QuoteStoreDiagnostic,
+    QuoteStoreDiagnosticCode,
+    QuoteStoreEvictionResult,
+    QuoteVersion,
+    RealtimeIngestionBatch,
+    RealtimeIngestionPolicy,
+    RealtimeIngestionRuntime,
+)
 
 __all__ = [
     "IngestedSnapshot",
     "IngestionBatch",
     "IngestionIssue",
+    "LiveQuoteStore",
+    "ProviderIngestionHealth",
+    "ProviderPollMetrics",
+    "ProviderRateGate",
+    "QuoteKey",
+    "QuoteStoreApplyResult",
+    "QuoteStoreDiagnostic",
+    "QuoteStoreDiagnosticCode",
+    "QuoteStoreEvictionResult",
+    "QuoteVersion",
+    "RealtimeIngestionBatch",
+    "RealtimeIngestionPolicy",
+    "RealtimeIngestionRuntime",
     "collect_snapshots",
 ]
